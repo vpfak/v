1714af7dd7af4c381e5a3a847cfa43a3cc2dd708
tfpak <48423431+tfpak@users.noreply.github.com>
2019-03-19 14:55:00 +0800

    Add files via upload

---
+++ js/css/cda.js
@@ -0,0 +1,13 @@
+﻿/*! cda v1.0.0 | (c) 2019年3月5日, 2019年3月18日 唐方鹏, Inc. |  */
+document.writeln("
+$("#divx").click(function(){
+  $("#divy").fadeIn();
+  $("#divx").fadeOut();
+}),
+
+$("#divy").click(function(){
+  $("#divy").fadeOut();
+  $("#divx").fadeIn();
+}),
+
+");
---
+++ js/css/cda1.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+/*! cda v1.0.0 | (c) 2019年3月5日, 2019年3月18日 唐方鹏, Inc. |  */
+document.writeln("
+$("#divx").click(function(){
+  $("#divy").fadeIn();
+  $("#divx").fadeOut();
+});
+
+$("#divy").click(function(){
+  $("#divy").fadeOut();
+  $("#divx").fadeIn();
+});
+
+");
